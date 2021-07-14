392cf3e9911f1ee13adda872b1331d6bb71c7347
Marco van Dijk <marco@dijk.tech>
2021-07-14 16:57:27 +0200

    More dark and blue

---
+++ .bash_profile
@@ -0,0 +1,10 @@
+#
+# ~/.bash_profile
+#
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
+#startx
+if [ -n "$DESKTOP_SESSION" ];then
+    eval $(gnome-keyring-daemon --start)
+    export SSH_AUTH_SOCK
+fi
--- .config/wofi/wofi-power.sh
+++ .config/wofi/wofi-power.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 
-poweroff_command="systemctl poweroff"
-reboot_command="systemctl reboot"
-logout_command="swaymsg exit"
-hibernate_command="systemctl hibernate"
-suspend_command="systemctl suspend"
+Shutdown_command="systemctl poweroff"
+Reboot_command="systemctl reboot"
+Logout_command="swaymsg exit"
+Hibernate_command="systemctl hibernate"
+Suspend_command="systemctl suspend"
+Back_command=""
 
 # you can customise the rofi command all you want ...
-rofi_command="rofi -width 10 -hide-scrollbar -bg #586e75 -opacity 100 -padding 5 -theme ~/.config/wofi/launcher.rasi"
-options=$'poweroff\nreboot\nlogout\nhibernate\nsuspend' 
+rofi_command="rofi -theme /home/marco/.config/wofi/launcherSmoll.rasi"
+options=$'Back\nShutdown\nLogout\nReboot\nHibernate\nSuspend' 
 
 # ... because the essential options (-dmenu and -p) are added here
 eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
